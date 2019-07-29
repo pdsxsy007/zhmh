c5b2ecd3247909b89a020ccd3575b0ced1eff90e
xsy <303775564@qq.com>
2019-07-29 15:17:53 +0800

    201907291517

--- app/src/main/java/io/cordova/zhqy/utils/MyApp.java
+++ app/src/main/java/io/cordova/zhqy/utils/MyApp.java
@@ -5,6 +5,9 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.multidex.MultiDex;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import cn.jiguang.verifysdk.api.JVerificationInterface;
 import cn.jpush.android.api.JPushInterface;
 import io.cordova.zhqy.AppException;
 import io.cordova.zhqy.bean.Constants;
@@ -52,6 +56,7 @@ public class MyApp extends Application {
         SPUtils.put(instance,"imei",imei);
         AppException appException = AppException.getInstance();
         appException.init(instance);
+        String registrationID = JPushInterface.getRegistrationID(instance);
 
 
         //友盟三方登录
@@ -72,9 +77,36 @@ public class MyApp extends Application {
         AligreenSdkManager.getInstance().init(instance);*/
 
         //本机号码一键登录
-       /* JVerificationInterface.setDebugMode(true);
-        JVerificationInterface.init(instance);*/
+        JVerificationInterface.setDebugMode(true);
+        JVerificationInterface.init(instance);
         //AllowX509TrustManager.allowAllSSL();
+
+        Cockroach.install(new Cockroach.ExceptionHandler() {
+
+            // handlerException内部建议手动try{  你的异常处理逻辑  }catch(Throwable e){ } ，以防handlerException内部再次抛出异常，导致循环调用handlerException
+
+            @Override
+            public void handlerException(final Thread thread, final Throwable throwable) {
+                //开发时使用Cockroach可能不容易发现bug，所以建议开发阶段在handlerException中用Toast谈个提示框，
+                //由于handlerException可能运行在非ui线程中，Toast又需要在主线程，所以new了一个new Handler(Looper.getMainLooper())，
+                //所以千万不要在下面的run方法中执行耗时操作，因为run已经运行在了ui线程中。
+                //new Handler(Looper.getMainLooper())只是为了能弹出个toast，并无其他用途
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                           /* Log.e("AndroidRuntime","--->CockroachException:"+thread+"<---",throwable);
+                            Toast.makeText(App.this, "Exception Happend\n" + thread + "\n" + throwable.toString(), Toast.LENGTH_SHORT).show();*/
+
+                        } catch (Throwable e) {
+
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
 
@@ -84,6 +116,7 @@ public class MyApp extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+        MultiDex.install(base);
     }
 
     public void getScreen(Context aty) {
